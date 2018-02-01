140d23ed56eb72440e06e848c0c72cad6ba1eeb0
Mary <schmitt.mary7@gmail.com>
2018-02-01 08:41:45 -0700

    updated paragraph

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
     <h1>Why we write setup methods in database unit tests</h1>
-    <p>You write the setup method so that you do not have to repeat as much code. It does the same thing before each unit test - in our example it wipes the database and inserts test data before each test.</p>
+    <p>You write the setup method so that you do not have to repeat as much code. It does the same thing before each unit test - in our example it wipes the database and inserts test data before each test. Makes the code "DRY -er".</p>
     <p>You also must make sure to call the setup method as a parent (wording here is probably wrong....) so that you do not override defaults - you only amend them.</p>
 </body>
 </html>
